4c5b95ea7fc9c82fd4af9dcd0b49cd590e268157
NatanaelNeves <natanaelnevesalves@gmail.com>
2025-06-06 16:56:50 -0300

    Refactor Firebase configuration to use environment variables for sensitive credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,18 @@
 // src/firebase.js
-
-// 1. IMPORTAR TUDO QUE PRECISAMOS
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Suas credenciais do NOVO projeto
+// O objeto agora lê as variáveis de ambiente
 const firebaseConfig = {
-  apiKey: "AIzaSyCZGXCFOaKitaUlVcOIyTwweuL-ijx6v_Q",
-  authDomain: "meu-app-treinos-final.firebaseapp.com",
-  projectId: "meu-app-treinos-final",
-  // 2. CORREÇÃO CRÍTICA AQUI:
-  storageBucket: "meu-app-treinos-final.appspot.com",
-  messagingSenderId: "275965865835",
-  appId: "1:275965865835:web:99fdc661ff23916ea7662e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// 3. EXPORTAR OS SERVIÇOS PARA O RESTO DO APP USAR
 export const auth = getAuth(app);
 export const db = getFirestore(app);
